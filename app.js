36b640538f4d96ceb4f47e33d134e2e66280d35d
Taushik Ahmed Joy <taushikjoy@gmail.com>
2021-08-20 21:26:31 +0600

    testing

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 window.addEventListener('load' , () => {
 
-  
+  let bodyy = document.querySelector('body') 
   let temparature = document.querySelector('.temp-degee');
   let location = document.querySelector('.location-timezone');
   let description = document.querySelector('.temp-description');
@@ -10,7 +10,7 @@ window.addEventListener('load' , () => {
   let city ;
 
   searchButton.addEventListener('click' , function () {
-    city = String(searchValue.value); 
+    city = searchValue.value; 
   //  })
 
   const api = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d968d0bf8e4b382411e3846f6e87d970` ;
@@ -28,13 +28,15 @@ window.addEventListener('load' , () => {
     const {name} = data;
 
 
-    temparature.textContent = temp/10;
+    temparature.textContent = temp - 273.15;
 
     location.textContent = name;
 
   if (temparature.textContent <32 && temparature.textContent >25 ) {
 
     description.textContent = "its a nice weather";
+    bodyy.style.background =  linearGradient(rgb(47,158,1), rgb(48,62,190)) ;
+    
 
   }
   else if(temparature.textContent >32 ) {
@@ -43,9 +45,9 @@ window.addEventListener('load' , () => {
 
   }
 
-   searchButton.addEventListener('click' , function () {
-    city == searchValue.textContent; 
-   })
+  //  searchButton.addEventListener('click' , function () {
+  //   city == searchValue.textContent; 
+  //  })
 
 
 
